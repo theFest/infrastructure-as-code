d41f3b30bcca9bd9e949f6502966d88492a02952
theFest <stjepanb83@gmail.com>
2023-01-15 04:20:15 +0100

    Initial dotnet-rg-webf-no_db-vnetpublic-ai-kv-redis-cosmos-gitops
    n/a atm

---
+++ Terraform/advanced-gitops/appservice/dotnet-rg-webf-no_db-vnetpublic-ai-kv-redis-cosmos-gitops/dotnet-rg-webf-no_db-vnetpublic-ai-kv-redis-cosmos-gitops.sh
@@ -0,0 +1,1 @@
+curl "https://nubesgen.com/docker-rg-webf-no_db-vnetpublic-ai-kv-redis-cosmos-gitops.tgz?iactool=TERRAFORM&region=westeurope&application=APP_SERVICE.free&runtime=DOCKER&database=NONE.basic&addons=application_insights,key_vault,storage_blob,redis,cosmosdb_mongodb&gitops=true" | tar -xzvf -
