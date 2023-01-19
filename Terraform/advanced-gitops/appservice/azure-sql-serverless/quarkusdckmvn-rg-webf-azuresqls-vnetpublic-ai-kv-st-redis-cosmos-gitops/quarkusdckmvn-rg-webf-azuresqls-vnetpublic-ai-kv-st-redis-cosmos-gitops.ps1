6d286d583fc18951b35730b01237f13e85a044b3
theFest <stjepanb83@gmail.com>
2023-01-19 23:12:11 +0100

    Revert "AzureSQL springbootmaven"
    This reverts commit d9c7f0b30d6f4d9120e8754e71b64a0c83b7174c.

--- Terraform/advanced-gitops/appservice/azure-sql-serverless/quarkusdckmvn-rg-webf-azuresqls-vnetpublic-ai-kv-st-redis-cosmos-gitops/quarkusdckmvn-rg-webf-azuresqls-vnetpublic-ai-kv-st-redis-cosmos-gitops.ps1
+++
@@ -1,1 +0,0 @@
-Invoke-WebRequest -Uri "https://nubesgen.com/test.zip?iactool=TERRAFORM&region=westeurope&application=APP_SERVICE.free&runtime=DOTNET&database=NONE.free&network=VIRTUAL_NETWORK" -OutFile test.zip; Expand-Archive test.zip -DestinationPath '.' -Force; Remove-Item test.zip
